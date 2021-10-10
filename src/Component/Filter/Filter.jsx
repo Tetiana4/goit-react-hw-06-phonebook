297387cf80af05a4b9c9e9495a46b11772d416ae
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-10-10 16:17:45 +0200

    Update Filter.jsx

--- src/Component/Filter/Filter.jsx
+++ src/Component/Filter/Filter.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import * as actions from '../../redux/actions'
 
 const Filter = ({ value, onChange }) => (
   <label>
     <input
       type="text"
       value={value}
-      onChange={event => onChange(event.currentTarget.value)}
+      onChange={onChange}
     />
   </label>
 );
-export default Filter;
+const mapStateToProps = (state) => ({
+  value: state.contacts.filter,
+})
+const mapDispatchToProps = dispatch => ({
+  onChange: (event) => dispatch(actions.filterContact(event.currentTarget.value))
+    
+  
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);
 
 Filter.propeTypes = {
   value: PropTypes.string,
